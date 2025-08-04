43e24e911c2daf94b9cfdbb704fdbfc1eaa1d6e3
Zohaib Ashraf <zohaibashraf2559@gmail.com>
2025-08-04 14:34:39 +0500

    Add files via upload

---
+++ Proj9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a, b;
+    cout << "Enter first number (a): ";
+    cin >> a;
+
+    cout << "Enter second number (b): ";
+    cin >> b;
+
+    cout << "Before swap: a = " << a << " , b = " << b <<endl;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After swap: a = " << a << " , b = " << b <<endl;
+    return 0;
+}
